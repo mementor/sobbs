71fbc99fcf9b14b6dc6b0fb1fc2e3eff5253256b
Ivan <null@binaryworld.ru>
2020-01-22 19:22:44 +0300

    race fixed

--- main.go
+++ main.go
@@ -400,7 +400,8 @@ func main() {
 	exitChan := make(chan bool, 1)
 
 	var wg sync.WaitGroup
-
+	wg.Add(threads)
+	
 	for i := 0; i < threads; i++ {
 		go worker(&wg, msgChan, exitChan)
 	}
@@ -447,7 +448,6 @@ func main() {
 
 	fmt.Fprintln(os.Stderr, "Cleaning...")
 	close(exitChan)
-	wg.Add(threads)
 	wg.Wait()
 }
 
